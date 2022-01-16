449136f16c5aed0f0a29b93f3bd12ec153bb0d36
Zhenyuan Gong <79224316+reC4P7CH4@users.noreply.github.com>
2022-01-16 12:52:15 -0500

    Update 45 - Jump Game II.cpp

--- 45 - Jump Game II.cpp
+++ 45 - Jump Game II.cpp
@@ -3,16 +3,15 @@
 using namespace std;
 
 class Solution {
-public:
+ public:
   int jump(vector<int>& nums) {
-    int Furthest=0,Steps=0,Begin=0,End=0;
-    const int TermCondi=nums.size()-1;
-    while(Furthest<TermCondi) {
-      for(int i=Begin;i<=End;++i)
-        Furthest=max(Furthest,i+nums[i]);
+    int Furthest = 0, Steps = 0, Begin = 0, End = 0;
+    const int TermCondi = nums.size() - 1;
+    while (Furthest < TermCondi) {
+      for (int i = Begin; i <= End; ++i) Furthest = max(Furthest, i + nums[i]);
       ++Steps;
-      Begin=End+1;
-      End=Furthest;
+      Begin = End + 1;
+      End = Furthest;
     }
     return Steps;
   }
