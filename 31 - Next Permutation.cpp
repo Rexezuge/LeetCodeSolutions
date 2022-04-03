8c087f851c8c8f9de5b1716e1580ea3e289ee309
Zhenyuan Gong <79224316+Rexezuge@users.noreply.github.com>
2022-04-03 15:21:45 -0400

    Update 31 - Next Permutation.cpp

--- 31 - Next Permutation.cpp
+++ 31 - Next Permutation.cpp
@@ -1,9 +1,10 @@
+#pragma GCC opptimize("fast")
 #include <bits/stdc++.h>
 using namespace std;
 
 class Solution {
  public:
-  void nextPermutation(vector<int>& nums) {
+  static void nextPermutation(vector<int>& nums) noexcept {
     next_permutation(nums.begin(), nums.end());
   }
 };
